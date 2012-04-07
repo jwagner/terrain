e6e5c20a8ceeb6d229b337cbf0a8ff9d99b0e58a
Jonas Wagner <jonas@29a.ch>
2012-04-07 16:47:38 +0200

    update release.sh

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ python build.py release
 cp build/main.js build/main_concat.js
 uglifyjs build/main_concat.js > build/main.js
 rm build/main_concat.js
-rsync -vrt --copy-links build/* 29a.ch:/var/www/29a.ch/sandbox/2011/addresscloud/
+rsync -vrt --copy-links build/* 29a.ch:/var/www/29a.ch/sandbox/2012/terrain/
 python build.py
