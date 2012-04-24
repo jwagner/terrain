80e0923e16a11640735e5579cc6078791dc2e379
Jonas Wagner <jonas@29a.ch>
2012-04-24 23:05:01 +0200

    Cleaned up release stuff

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ python build.py release
 cp build/main.js build/main_concat.js
 uglifyjs build/main_concat.js > build/main.js
 rm build/main_concat.js
-rsync -vrt --copy-links build/* 29a.ch:/var/www/29a.ch/sandbox/2012/terrain/
+rsync -vrt --copy-links --include gfx/height4k_diff.png --exclude "gfx/*" build/* 29a.ch:/var/www/29a.ch/sandbox/2012/terrain/
 python build.py
